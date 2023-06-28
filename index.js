d94a0946925ca4120880be981e35c750bcfe5f05
Rudra <merudramanu@gmail.com>
2023-06-28 12:47:26 +0530

    added cors updation

--- index.js
+++ index.js
@@ -3,6 +3,8 @@ require("dotenv").config();
 const express = require("express");
 const app = express();
 app.use(express.json());
+const cors = require("cors");
+app.use(cors());
 const configuration = new Configuration({
   apiKey: process.env.openAIkey,
 });
